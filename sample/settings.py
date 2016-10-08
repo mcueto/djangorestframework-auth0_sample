caa66c6cf74fd72a08758f0205caf59ea039f3f7
mcueto <cueto@live.cl>
2016-10-08 11:26:33 -0300

    minor changes

--- sample/settings.py
+++ sample/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
-from .settings_secret import *
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
