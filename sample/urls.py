09456d63fc0751b64728e14b3b491a3b313f0d3a
Marcelo Cueto <cueto@live.cl>
2020-01-08 23:19:19 -0300

    Refactor sample.urls submodule

--- sample/urls.py
+++ sample/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import (
+    url,
+    include,
+)
 from django.contrib import admin
 from rest_framework import routers
-from .views import ToDoViewSet
+from .views import (
+    ToDoViewSet,
+)
 
 router = routers.DefaultRouter()
 router.register(r'todos', ToDoViewSet)
