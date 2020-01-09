4edbf9476d5f665a644945e1c23830a1ce582afb
Marcelo Cueto <cueto@live.cl>
2020-01-08 23:52:16 -0300

    Create AllToDosViewSet to get all ToDo instances

--- sample/views.py
+++ sample/views.py
@@ -5,6 +5,15 @@ from rest_framework_auth0.permissions import HasAdminRole
 from rest_framework_auth0.views import GroupsQuerysetFilterMixin
 
 
+class AllToDosViewSet(viewsets.ModelViewSet):
+    """
+    AllToDosViewSet.
+    Retrieve a list with all ToDo model instances.
+    """
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
+
 class ToDoViewSet(GroupsQuerysetFilterMixin, viewsets.ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
