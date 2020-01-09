5a405aa94558d63b7f1961f5c8c5f89731228ecb
Marcelo Cueto <cueto@live.cl>
2020-01-09 00:13:59 -0300

    Create SecureToDosViewSet

--- sample/views.py
+++ sample/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import ToDo
 from .serializers import ToDoSerializer
+from rest_framework_auth0.authentication import Auth0JSONWebTokenAuthentication
 from rest_framework_auth0.permissions import HasAdminRole
 from rest_framework_auth0.views import GroupsQuerysetFilterMixin
 
@@ -12,6 +14,18 @@ class AllToDosViewSet(viewsets.ModelViewSet):
     """
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
+
+
+class SecureToDosViewSet(viewsets.ModelViewSet):
+    """
+    SecureToDosViewSet.
+    Retrieve a list with all ToDo model instances only when the user is
+    authenticated.
+    """
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+    authentication_classes = [Auth0JSONWebTokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class ToDoViewSet(GroupsQuerysetFilterMixin, viewsets.ModelViewSet):
