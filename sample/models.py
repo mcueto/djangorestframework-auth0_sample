f1205705569c42a7d4b5693c0bc480d7fa145915
Marcelo Cueto <cueto@live.cl>
2020-01-08 23:18:02 -0300

    Refactor sample.models submodule

--- sample/models.py
+++ sample/models.py
@@ -3,9 +3,15 @@ from django.db import models
 
 class ToDo(models.Model):
     text = models.TextField()
-    done = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    done = models.BooleanField(
+        default=False
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
 
     def __str__(self):
         return self.text
