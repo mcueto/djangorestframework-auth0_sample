125a38c12a8b6c64046deaa4a85571762be4a1a7
Marcelo Cueto <cueto@live.cl>
2020-01-19 23:22:59 -0300

    Add breakpoints on models and serializers submodules

--- sample/models.py
+++ sample/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 
+
 class ToDo(models.Model):
     text = models.TextField()
     done = models.BooleanField(
--- sample/serializers.py
+++ sample/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import ToDo
 
+
 class ToDoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ToDo
